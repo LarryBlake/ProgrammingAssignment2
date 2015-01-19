a7496c9e50c6b5157e34827c84017378c626247d
LarryBlake <lmmblake@live.com>
2015-01-19 11:19:38 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Assignment 2
+## Pair of functions to:
+##    1. Save a matrix / inversion pair for future retrieval
+##    2. Return the inversion of a matrix, using saved calculations if possible
 
-## Write a short comment describing this function
+## Write the matrix / inversion pair for future
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Does cached solution exist?  Return it.  Otherwise invert the matrix and save.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
