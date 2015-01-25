ae13376ca3a7e229f6728ddfc673aa86244ca62a
LarryBlake <lmmblake@live.com>
2015-01-25 16:35:08 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,27 @@
 ##    1. Save a matrix / inversion pair for future retrieval
 ##    2. Return the inversion of a matrix, using saved calculations if possible
 
+
 ## Write the matrix / inversion pair for future
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- solve(x)
+	
+	return(m)
 }
 
 
 ## Does cached solution exist?  Return it.  Otherwise invert the matrix and save.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	## look for cached
+	m <- x$getsolve
+	if (!is.null(m)) {
+		message("found in cache")
+		return(m)
+	}
+
+	## Not there.  Invert, save, return.
+	makeCacheMatrix(x)
 }
